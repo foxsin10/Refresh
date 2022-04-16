39ce30ca58f6075b1cd9938d8ed5ace756091297
foxsin10 <yzjcnn@gmail.com>
2022-04-16 16:52:25 +0800

    chore: exclude Demo project

--- Package.swift
+++ Package.swift
@@ -21,9 +21,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PullRefresh",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Demo"]
+        ),
         .testTarget(
             name: "PullRefreshTests",
-            dependencies: ["PullRefresh"]),
+            dependencies: ["PullRefresh"],
+            exclude: ["Demo"]
+        ),
     ]
 )
